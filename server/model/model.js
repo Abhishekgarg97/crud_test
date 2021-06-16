f0b6d9657c9279fbf46667d934b1e1ade67fefdb
Abhishekgarg97 <59972803+Abhishekgarg97@users.noreply.github.com>
2021-06-16 15:33:06 +0530

    testing phase1

---
+++ server/model/model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+var schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  gender: String,
+  status: String,
+  designation: String,
+});
+
+const userdb = mongoose.model("userdb", schema);
+
+module.exports = userdb;
