f45e571653baa126a74b5f0b2eed07c2f8ae48b6
Abhishekgarg97 <59972803+Abhishekgarg97@users.noreply.github.com>
2021-06-18 13:00:50 +0530

    errors in data validation

--- server/model/model.js
+++ server/model/model.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
 var schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  contact: {
-    type: String,
-    required: true,
-  },
-  gender: String,
-  status: String,
-  designation: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    contact: {
+        type: String,
+        required: true,
+    },
+    gender: String,
+    designation: String,
+    file: String,
 });
 
 const userdb = mongoose.model("userdb", schema);
